3fe014f790acc05ef43b9c6f11fc3330a551790d
Eric Bishard <e.bishard@gmail.com>
2022-01-29 10:07:18 -0800

    Remove Fragment

--- src/Wallet/Wallet.tsx
+++ src/Wallet/Wallet.tsx
@@ -78,36 +78,34 @@ const Wallet = () => {
   return (
     <>
       <Header />
-      <>
-        <div className="walletButtonContainer grid gap-y-4 sm:grid-cols-2 flex-1">
-          {(_.keys(connectorsByName) as ConnectorNames[]).map((name) => {
-            const currentConnector = connectorsByName[name];
-            const activating = currentConnector === activatingConnector;
-            const connected = currentConnector === connector;
-            const disabled = !!activatingConnector || connected || !!error;
-            const nameLookupKey = name.toLowerCase();
+      <div className="walletButtonContainer grid gap-y-4 sm:grid-cols-2 flex-1">
+        {(_.keys(connectorsByName) as ConnectorNames[]).map((name) => {
+          const currentConnector = connectorsByName[name];
+          const activating = currentConnector === activatingConnector;
+          const connected = currentConnector === connector;
+          const disabled = !!activatingConnector || connected || !!error;
+          const nameLookupKey = name.toLowerCase();
 
-            return (
-              <div className="mx-auto block w-full h-full text-center" key={name}>
-                <button className="network-btns text-center relative block w-full h-full" type="button"
-                  disabled={disabled}
-                  onClick={() => {
-                    setActivatingConnector(currentConnector);
-                    activate(connectorsByName[name]);
-                  }}
-                >
-                  {activating && (
-                    <Spinner className="top-1/2 left-1/2 absolute transform -translate-x-1/2 -translate-y-1/2 h-8 z-10" />
-                  )}
-                  <img className="mx-auto mb-2" src={walletMeta[nameLookupKey]?.uri} alt="" role="presentation" style={{ maxHeight: "70px" }} />
-                  <h3 className="header5 font-sans">{name}</h3>
-                  <h4 className="web3-block-description">{walletMeta[nameLookupKey]?.description}</h4>
-                </button>
-              </div>
-            );
-          })}
-        </div>
-      </>
+          return (
+            <div className="mx-auto block w-full h-full text-center" key={name}>
+              <button className="network-btns text-center relative block w-full h-full" type="button"
+                disabled={disabled}
+                onClick={() => {
+                  setActivatingConnector(currentConnector);
+                  activate(connectorsByName[name]);
+                }}
+              >
+                {activating && (
+                  <Spinner className="top-1/2 left-1/2 absolute transform -translate-x-1/2 -translate-y-1/2 h-8 z-10" />
+                )}
+                <img className="mx-auto mb-2" src={walletMeta[nameLookupKey]?.uri} alt="" role="presentation" style={{ maxHeight: "70px" }} />
+                <h3 className="header5 font-sans">{name}</h3>
+                <h4 className="web3-block-description">{walletMeta[nameLookupKey]?.description}</h4>
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }

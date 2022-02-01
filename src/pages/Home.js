091cdce00d3bb582dc25f04398380678af6a8faa
dinadeljanin <dinadeljanin@protonmail.com>
2022-02-01 01:53:00 -0500

    Remove unused vars

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,4 @@
 import { useEffect, useState, useContext } from "react";
-import { ellipseAddress } from "../lib/utils";
-import { useWeb3React, UnsupportedChainIdError } from "@web3-react/core";
-import { Web3Provider } from "@ethersproject/providers";
 
 import { ViewContext } from "../context/AppContext"
 
@@ -12,21 +9,9 @@ import ArbitrumConnect from "Wallet/ArbitrumConnect";
 import GetTokens from "Wallet/GetTokens";
 
 const Home = () => {
-  // const context = useWeb3React();
-  // const { active, error, account, library, connector } = context;
-  // const [activatingConnector, setActivatingConnector] = useState();
-  // const unsupportedChain = error instanceof UnsupportedChainIdError
 
-	const { user, provider, contracts, state, dispatch, chainId } = useContext(ViewContext)
+	const { user, provider, chainId } = useContext(ViewContext)
   const { address } = user
-
-  // useEffect(() => {
-  //   if (activatingConnector && activatingConnector === connector) {
-  //     setActivatingConnector(undefined);
-  //   }
-  // }, [activatingConnector, connector]);
-
-  console.log(chainId)
 
   const renderView = () => {
     switch (true) {
@@ -49,14 +34,7 @@ const Home = () => {
       </header>
       <main className="flex-grow relative">
         <div className="main-content shadow absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white px-6 py-10 sm:px-4 rounded flex flex-col">
-        {renderView()}
-          {/* {
-            unsupportedChain
-              ? <ArbitrumConnect />
-              : !active
-                ? <Wallet />
-                : active && <GetTokens />
-          } */}
+          {renderView()}
         </div>
       </main>
     </div>
